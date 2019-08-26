840fa6ab8f3a82cc5288b6476302b0d117cdec12
Floris Velleman <florisvelleman@hotmail.com>
2019-08-26 21:19:09 +0200

    Moved header into seperate component.

--- ArenaTactics/src/app/app.module.ts
+++ ArenaTactics/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     MenuComponent,
     ContentComponent
   ],
